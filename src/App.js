2195a2d22dc97402067439ca2838ac2022ed9393
Lucas Hoang Nguyen <hoangknguyen.1807@gmail.com>
2022-04-12 17:48:24 +0700

    Update state of component every second

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      Online Department store
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {today: new Date()};
+  }
+
+  componentDidMount() {
+    this.taskId = setInterval(this.tick, 1000); 
+  }
+
+  tick = () => this.setState({today: new Date()});
+
+  componentWillUnmount() {
+    clearInterval(this.taskId);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Online Department Store
+        </h1>
+        <h3>
+          It is now {this.state.today.toLocaleTimeString('en-gb')}, {this.state.today.toLocaleDateString('en-gb')}.
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default App;
